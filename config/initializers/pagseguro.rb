b21fe86e670834618212cbfb7d423804079afbbe
cesarfuster <cfuster@uol.com.br>
2018-06-30 07:43:46 -0300

    Initializer pagseguro with token

---
+++ config/initializers/pagseguro.rb
@@ -0,0 +1,6 @@
+PagSeguro.configure do |config|
+  config.token       = "762DE632F7C9476E8C637B4CD0397BB2"
+  config.email       = "cfuster@uol.com.br"
+  config.environment = :sandbox # ou :sandbox. O padrão é production.
+  config.encoding    = "UTF-8"
+end
